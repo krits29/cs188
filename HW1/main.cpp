7856ca2710b2ecb6353a886c244b2446900032c3
Yogesh Nath <ynsunny@yahoo.com>
2017-03-07 18:00:49 -0800

    Initial Commit

---
+++ HW1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW1
+//
+//  Created by Kriti Sharma on 3/7/17.
+//  Copyright © 2017 Kriti Sharma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
